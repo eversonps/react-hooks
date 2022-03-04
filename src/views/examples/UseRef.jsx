10249834790369b873630457d2a25043737d1e03
Everson <eversonpereiraofc@gmail.com>
2022-03-04 15:30:25 -0300

    Complementando useRef e realizando desafio com minha resolução

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -2,13 +2,52 @@ import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+const merge = function(s1, s2) {
+    let v1 = s1.split("")
+    let v2 = s2.split("")
+    let vResult = []
+    let lengthFor
+
+    if(v1.length > v2.length){
+        lengthFor = v1.length
+    }else{
+        lengthFor = v2.length
+    }
+
+    for(let i=0; i <= lengthFor; i++){
+        if(v1[i]){
+            vResult.push(v1[i])
+        }
+
+        if(v2[i]){
+            vResult.push(v2[i])
+        }
+    }
+
+    return vResult.join("")
+}
+
 const UseRef = (props) => {
     const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
+    const [string, setString] = useState("")
+
     const count = useRef(0)
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
 
     useEffect(function(){
-        count.current = count.current + 1
+        count.current++
+        myInput2.current.focus()
+        setString(merge(myInput1.current.value, myInput2.current.value))
     }, [value1])
+
+    useEffect(function(){
+        count.current++
+        myInput1.current.focus()
+        setString(merge(myInput1.current.value, myInput2.current.value))
+    }, [value2])
+
     return (
         <div className="UseRef">
             <PageTitle
@@ -20,12 +59,17 @@ const UseRef = (props) => {
             <div className="center">
                 <div>
                     <span className="text">Valor: </span>
-                    <span className="text">{value1} [</span>
+                    <span className="text">{string} [</span>
                     <span className="text red">{count.current}</span>
                     <span className="text">]</span>
                 </div>
                 
-                <input type="text" className="input" value={value1} onChange={e => setValue1(e.target.value)}/>
+                <input ref={myInput1} type="text" className="input" value={value1} onChange={e => setValue1(e.target.value)}/>
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <input ref={myInput2} type="text" className="input" value={value2} onChange={e => setValue2(e.target.value)}/>
             </div>
         </div>
     )
