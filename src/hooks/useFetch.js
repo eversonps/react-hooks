d6d7ff9c621b349b5a1a93d2dcb74d8abd8e1d07
Everson Pereira <eversonpereiraofc@gmail.com>
2022-03-06 16:53:28 -0300

    utilizando hooks customizados

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(url, method = "GET"){
+    const [response, setResponse] = useState({
+        data: null,
+        loading: true
+    })
+
+    useEffect(function(){
+        fetch(url,{
+            method
+        })
+        .then(resp=>resp.json())
+        .then(json=> setResponse({
+            data: json,
+            loading: false
+        }))
+    }, [url, method])
+
+    return response
+} 
